6a0963e882caa9750049a0d2c95222ceabba0839
Karen Chang <71556642+kkkchch@users.noreply.github.com>
2020-09-26 21:07:25 +0800

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+hourly_salary= 120#時薪
+annual_salary= hourly_salary*8*365#年薪
+expenses= (3000+10000)*12#支出
+accumulation= annual_salary-expenses #積蓄 
+print (accumulation)
+print ("今天開始入門了")#註解說明 
+#123456789
+'''
+今天
+明天
+'''
+"""
+rup
+ripo
+"""
+
+a=20
+b=a/2
+print(b)
