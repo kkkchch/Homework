83289df62d08db735aee6d932d190ed7e7453c36
Karen Chang <71556642+kkkchch@users.noreply.github.com>
2020-10-01 00:43:34 +0800

    Add files via upload

---
+++ HW3_1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 22:34:29 2020
+
+@author: User
+"""
+r=5
+Pi=3.14159
+area=r**2*Pi
+print (area)
+#圓面積
+circlering=2*r*Pi
+print (circlering)
+#圓周長
