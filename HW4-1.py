cddff4210de74cbf7e7473e6a9e0f56a0cb01c99
Karen Chang <71556642+kkkchch@users.noreply.github.com>
2020-10-17 00:26:34 +0800

    Add files via upload

---
+++ HW4-1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 16 23:56:17 2020
+
+@author: User
+"""
+hourspeed=1225
+daykim=1225*24
+distance=384400
+day= distance/daykim
+hr= distance/hourspeed
+print (day)
+print(hr)
---
+++ HW4-2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 17 00:06:26 2020
+
+@author: User
+"""
+print (ord('K'),ord('A'),ord('R'),ord('E'),ord('N'))
