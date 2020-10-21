fbafc0b9b76dfd55451998a72b4053364dabd9b9
Karen Chang <71556642+kkkchch@users.noreply.github.com>
2020-10-21 23:59:37 +0800

    Add files via upload

---
+++ HW5-2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 23:44:19 2020
+
+@author: User
+"""
+file_159= open(c:\pythontest\out1.txt,mode="a")
+name= input ("姓名:")
+bd= input ("生日:")
+old= input ("年齡:")
+print ("Testing for output,", file= file_159)
+file_159.close ()
