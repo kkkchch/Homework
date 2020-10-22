3ef9d596f3f1790ec64412a65ede72e097345371
Karen Chang <71556642+kkkchch@users.noreply.github.com>
2020-10-22 18:57:27 +0800

    Add files via upload

---
+++ HW5-3.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 23:54:18 2020
+
+@author: User
+"""
+file20 = open ("c:\pythontest\out2.txt", mode="a")
+print("Hihihhihih",end="", file=file20)
+file20.close()
