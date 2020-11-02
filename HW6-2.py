6617fe2325c6d01f09aeca7252bdfd20d5e026a5
Karen Chang <71556642+kkkchch@users.noreply.github.com>
2020-11-02 23:16:15 +0800

    Add files via upload

---
+++ HW6-2.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 22:57:41 2020
+
+@author: User
+"""
+A= input("請輸入出生西元年(以1900年後為主):")
+animal= (int(A)-1900)%12
+if (int(animal)==1):
+    print ("您的生肖為鼠")
+if (int(animal)==2):
+    print ("您的生肖為牛")
+if (int(animal)==3):
+    print ("您的生肖為虎")
+if (int(animal)==4):
+    print ("您的生肖為兔")    
+if (int(animal)==5):
+    print ("您的生肖為龍")
+if (int(animal)==6):
+    print ("您的生肖為蛇")
+if (int(animal)==7):
+    print ("您的生肖為馬")
+if (int(animal)==8):
+    print ("您的生肖為羊")     
+if (int(animal)==9):
+    print ("您的生肖為猴")
+if (int(animal)==10):
+    print ("您的生肖為雞")
+if (int(animal)==11):
+    print ("您的生肖為狗")
+if (int(animal)==0):
+    print ("您的生肖為豬")     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
