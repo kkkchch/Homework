5d09f13f1f2a828b95005be7be40bd644bfd85bb
Karen Chang <71556642+kkkchch@users.noreply.github.com>
2020-12-20 23:03:42 +0800

    Add files via upload

---
+++ HW9-2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 20 21:02:40 2020
+
+@author: User
+"""
+n=1
+weight=int(input("請輸入體重:"))
+while n <=10:
+    weight=1.5*n+50
+    n+=1
+    print (weight)
